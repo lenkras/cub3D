c50e0f9ae02ebe7f2396b46ecebf59e0b1565c1a
Elena <yelenakras@gmail.com>
2024-09-27 13:21:04 +0300

    add RGB splitting, modify path representation

--- cub.h
+++ cub.h
@@ -33,8 +33,28 @@ typedef struct  s_cub
 	char	*EA_array;
 	char	*F_array;
 	char	*C_array;
+	int		F_R;
+	int		F_G;
+	int		F_B;
+	int		C_R;
+	int		C_G;
+	int		C_B;
 
 
 } t_cub;
+
+void	free_array(char **arr);
+char	*open_file(char *argv);
+
+void	north_array(char *line, t_cub *cub);
+void	south_array(char *line, t_cub *cub);
+void	west_array(char *line, t_cub *cub);
+void	east_array(char *line, t_cub *cub);
+void	split_toRGB_floor(char *array, t_cub *cub);
+void	split_toRGB_ceiling(char *array, t_cub *cub);
+void	floor_array(char *line, t_cub *cub);
+void	ceiling_array(char *line, t_cub *cub);
+void	copy_map(char **array, t_cub *cub, int start);
+int		find_map_start(char *line);
 
 #endif
