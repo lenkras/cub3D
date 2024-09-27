9b616ddb72f1a4f2a86962f69fab0b896000711e
Elena <yelenakras@gmail.com>
2024-09-27 14:51:05 +0300

    add initialisation, refactor code

--- main.c
+++ main.c
@@ -12,7 +12,25 @@
 
 #include "cub.h"
 
-void	split_by_new_line(t_cub *cub)
+int	check_format(char *file_name)
+{
+	char	*compare;
+
+	compare = ft_strrchr(file_name, '.');
+	if (!compare)
+	{
+		perror("Error: Invalid map file extansion.");
+		return (1);
+	}
+	if (ft_strncmp(compare, ".cub", 5) != 0)
+	{
+		perror("Error: Invalid map file extansion.");
+		return (1) ;
+	}
+	return (0);
+}
+
+int	split_by_new_line(t_cub *cub)
 {
 	char	**array;
 	int		i;
@@ -21,27 +39,36 @@ void	split_by_new_line(t_cub *cub)
 	array = ft_split(cub->file, '\n');
 	if (!array)
 	{
-		perror("Failed to split by new line.");
-		return ;
+		perror("Error: Failed to split by new line.");
+		return (1);
 	}
 	for (int i = 0; array[i] != NULL; i++)
 		printf("splited str %d: %s\n", i, array[i]);
+	init_flag_struct(cub);
 	while (array[i])
 	{
-		north_array(array[i], cub);
-		south_array(array[i], cub);
-		west_array(array[i], cub);
-		east_array(array[i], cub);
-		floor_array(array[i], cub);
-		ceiling_array(array[i], cub);
+		if (north_array(array[i], cub) == 1 || south_array(array[i], cub) == 1 || west_array(array[i], cub) == 1 ||
+			east_array(array[i], cub) == 1 || floor_array(array[i], cub) == 1 || ceiling_array(array[i], cub) == 1)
+			return (1);
+		// north_array(array[i], cub);
+		// south_array(array[i], cub);
+		// west_array(array[i], cub);
+		// east_array(array[i], cub);
+		// floor_array(array[i], cub);
+		// ceiling_array(array[i], cub);
 		if (find_map_start(array[i]))
 			break ;
 		i++;
 	}
+	// if (check_all_flags_infile(cub) == 1)
+	// {
+	// 	perror("Invalid map content.");
+	// 	return (1);
+	// }
 	copy_map(array, cub, i);
 	free_array(array);
+	return (0);
 }
-
 
 int main(int argc, char **argv)
 {
@@ -49,19 +76,23 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		perror("Wrong number of arguments.");
+		perror("Error: Wrong number of arguments.");
 		return (1);
 	}
+	if (check_format(argv[1]) == 1)
+		return (1);
 	else
 	{
 		cub.file = open_file(argv[1]);
 		printf("%s\n",cub.file);
 		if (!cub.file)
 		{
-			perror("Failed to open or read file.");
+			perror("Error: Failed to open or read file.");
 			return (1);
 		}
-		split_by_new_line(&cub);
+		init_main_struct(&cub);
+		if (split_by_new_line(&cub) == 1)
+			return (1);
 	}
 	printf("Map:\n");
     for (int i = 0; cub.map && cub.map[i] != NULL; i++)
---
+++ parse_world_sides.c
@@ -0,0 +1,157 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_world_sides.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epolkhov <epolkhov@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-09-27 11:47:45 by epolkhov          #+#    #+#             */
+/*   Updated: 2024-09-27 11:47:45 by epolkhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub.h"
+
+int	north_array(char *line, t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	start;
+
+	i = 0;
+	if (ft_strncmp(line, "NO", 2) == 0 && cub->flags.NO_flag == 0)
+	{
+		while(line[i] && (line[i] != '.' && line[i] != '/'))
+			i++;
+		start = i;
+		while (line[i] && line[i] != ' ' && line[i] != '\n')
+			i++;
+		cub->NO_array = malloc(i - start + 1);
+		if (!cub->NO_array)
+		{
+			perror("Error: Failed to allocate mamory.");
+			return (1);
+		}
+		j = 0;
+		while (start < i)
+			cub->NO_array[j++] = line[start++];
+		cub->NO_array[j] = '\0';
+		printf("NO_array: %s\n", cub->NO_array);
+		cub->flags.NO_flag = 1;
+		return (0);
+	}
+	if (ft_strncmp(line, "NO", 2) == 0 && cub->flags.NO_flag == 1)
+	{
+		perror("Error: Invalid map content.");
+		return (1);
+	}
+	return (0);
+}
+
+int	south_array(char *line, t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	start;
+
+	i = 0;
+	if (ft_strncmp(line, "SO", 2) == 0 && cub->flags.SO_flag == 0)
+	{
+		while(line[i] && (line[i] != '.' && line[i] != '/'))
+			i++;
+		start = i;
+		while (line[i] && line[i] != ' ' && line[i] != '\n')
+			i++;
+		cub->SO_array = malloc(i - start + 1);
+		if (!cub->NO_array)
+		{
+			perror("Error: Failed to allocate mamory.");
+			return (1);
+		}
+		j = 0;
+		while (start < i)
+			cub->SO_array[j++] = line[start++];
+		cub->SO_array[j] = '\0';
+		printf("SO_array: %s\n", cub->SO_array);
+		cub->flags.SO_flag = 1;
+		return (0);
+	}
+	if (ft_strncmp(line, "SO", 2) == 0 && cub->flags.SO_flag == 1)
+	{
+		perror("Error: Invalid map content.");
+		return (1);
+	}
+	return (0);
+}
+
+int	west_array(char *line, t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	start;
+
+	i = 0;
+	if (ft_strncmp(line, "WE", 2) == 0 && cub->flags.WE_flag == 0)
+	{
+		while(line[i] && (line[i] != '.' && line[i] != '/'))
+			i++;
+		start = i;
+		while (line[i] && line[i] != ' ' && line[i] != '\n')
+			i++;
+		cub->WE_array = malloc(i - start + 1);
+		if (!cub->NO_array)
+		{
+			perror("Error: Failed to allocate mamory.");
+			return (1);
+		}
+		j = 0;
+		while (start < i)
+			cub->WE_array[j++] = line[start++];
+		cub->WE_array[j] = '\0';
+		printf("WE_array: %s\n", cub->WE_array);
+		cub->flags.WE_flag = 1;
+		return (0);
+	}
+	if (ft_strncmp(line, "WE", 2) == 0 && cub->flags.WE_flag == 0)
+	{
+		perror("Error: Invalid map content.");
+		return (1);
+	}
+	return (0);
+}
+
+int	east_array(char *line, t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	start;
+
+	i = 0;
+	if (ft_strncmp(line, "EA", 2) == 0 && cub->flags.EA_flag == 0)
+	{
+		while(line[i] && (line[i] != '.' && line[i] != '/'))
+			i++;
+		start = i;
+		while (line[i] && line[i] != ' ' && line[i] != '\n')
+			i++;
+		cub->EA_array = malloc(i - start + 1);
+		if (!cub->EA_array)
+		{
+			perror("Error: Failed to allocate mamory.");
+			return (1);
+		}
+		j = 0;
+		while (start < i)
+			cub->EA_array[j++] = line[start++];
+		cub->EA_array[j] = '\0';
+		printf("EA_array: %s\n", cub->EA_array);
+		cub->flags.EA_flag = 1;
+		return (0);
+	}
+	if (ft_strncmp(line, "EA", 2) == 0 && cub->flags.EA_flag == 1)
+	{
+		perror("Error: Invalid map content.");
+		return (1);
+	}
+	return (0);
+}
